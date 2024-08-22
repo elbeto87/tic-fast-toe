7128c2029c9d60cbc97efd442991da0961508ab5
Gresia, Ezequiel <Ezequiel.Gresia@gilbarco.com>
2024-08-22 14:40:11 -0300

    First TicFastToe version

--- main.py
+++ main.py
@@ -6,10 +6,33 @@ from tic_tac_toe import TicTacToe
 app = FastAPI()
 
 
-@app.get("/start_game")
+tic_tac_toe = TicTacToe()
+
+
+@app.post("/start_game")
 def start_game():
-    logger.info("Starting Tic Tac Toe Game")
     return {
-        "message": "Welcome to Tic Fast Toe",
-        "board": TicTacToe().start_game()
+        "message": f"Welcome to Tic Fast Toe",
+        "board": tic_tac_toe.start_game()
+    }
+
+
+@app.post("/make_move")
+def make_move(row: int, col: int):
+    logger.info(f"Making move at row: {row} and column: {col}")
+    tic_tac_toe.make_player_move(row, col)
+    if tic_tac_toe.is_a_winner(tic_tac_toe.player):
+        return {
+            "message": f"Player {tic_tac_toe.player} wins!",
+            "board": tic_tac_toe.print_board()
+        }
+    tic_tac_toe.make_computer_move()
+    if tic_tac_toe.is_a_winner(tic_tac_toe.computer):
+        return {
+            "message": f"Player {tic_tac_toe.computer} wins!",
+            "board": tic_tac_toe.print_board()
+        }
+    return {
+        "message": "Move made successfully",
+        "board": tic_tac_toe.print_board()
     }
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,11 @@
+import random
+
+
 class TicTacToe:
 
-    def __init__(self, player_1: str = "X", player_2: str = "O"):
-        self.player_1 = player_1
-        self.player_2 = player_2
+    def __init__(self):
+        self.player = "X"
+        self.computer = "O"
         self.board = None
 
     def reset_board(self):
@@ -19,3 +22,27 @@ class TicTacToe:
     def start_game(self):
         self.reset_board()
         return self.print_board()
+
+    def make_player_move(self, row: int, col: int):
+        self.board[row-1][col-1] = self.player
+        return self.print_board()
+
+    def make_computer_move(self):
+        random_row = random.randint(0, 2)
+        random_col = random.randint(0, 2)
+        while self.board[random_row][random_col] != "_":
+            random_row = random.randint(0, 2)
+            random_col = random.randint(0, 2)
+        self.board[random_row][random_col] = self.computer
+
+    def is_a_winner(self, player):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] == player:
+                return True
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] == player:
+                return True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == player:
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] == player:
+            return True
+        return False
