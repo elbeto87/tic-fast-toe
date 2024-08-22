c6544ae6b30f7fc362ff0f167baa1c5b2897768b
Gresia, Ezequiel <Ezequiel.Gresia@gilbarco.com>
2024-08-22 14:54:22 -0300

    Implementing Docker and Pydantic library for models

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from logger import logger
 from tic_tac_toe import TicTacToe
@@ -9,7 +10,12 @@ app = FastAPI()
 tic_tac_toe = TicTacToe()
 
 
-@app.post("/start_game")
+class MakeMoveRequest(BaseModel):
+    row: int
+    col: int
+
+
+@app.post("/")
 def start_game():
     return {
         "message": f"Welcome to Tic Fast Toe",
@@ -18,15 +24,18 @@ def start_game():
 
 
 @app.post("/make_move")
-def make_move(row: int, col: int):
-    logger.info(f"Making move at row: {row} and column: {col}")
-    tic_tac_toe.make_player_move(row, col)
+def make_move(make_move: MakeMoveRequest):
+    logger.info(f"Player is making a move at row: {make_move.row} and column: {make_move.col}")
+    tic_tac_toe.make_player_move(make_move.row, make_move.col)
+    logger.info(f"Verifying if player {tic_tac_toe.player} is a winner")
     if tic_tac_toe.is_a_winner(tic_tac_toe.player):
         return {
             "message": f"Player {tic_tac_toe.player} wins!",
             "board": tic_tac_toe.print_board()
         }
+    logger.info(f"Computer is making a random move")
     tic_tac_toe.make_computer_move()
+    logger.info(f"Verifying if player {tic_tac_toe.computer} is a winner")
     if tic_tac_toe.is_a_winner(tic_tac_toe.computer):
         return {
             "message": f"Player {tic_tac_toe.computer} wins!",
