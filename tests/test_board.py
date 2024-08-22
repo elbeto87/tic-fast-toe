fb4685bbc231f1a6d3671d10e054da90ccbf5913
Gresia, Ezequiel <Ezequiel.Gresia@gilbarco.com>
2024-08-22 15:56:06 -0300

    Adding a test

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from http import HTTPStatus
 from main import app
@@ -5,11 +6,24 @@ from main import app
 client = TestClient(app)
 
 
-def test_start_game_endpoint():
-    response = client.get("/start_game")
+@pytest.fixture(scope="function")
+def new_game():
+    client.post("/")
+
+
+def test_start_game_endpoint(new_game):
+    response = client.post("/")
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         "message": "Welcome to Tic Fast Toe",
         "board": ["_ | _ | _", "_ | _ | _", "_ | _ | _"]
     }
+
+
+def test_make_move_request_endpoint(new_game):
+    response = client.post("/make_move_request", json={"row": 1, "col": 1})
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()["message"] == "Move made successfully"
+    assert response.json()["board"][0][0] == "X"
